ff60f2add41f810d0b67ff8437a9a204cf91390d
SaraJovanovska <sara.jovanovska.bt@hotmail.com>
2015-03-04 16:51:05 +0100

    RegistrationPage

---
+++ FIKT.SwmEasy/Registration.aspx.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+
+public partial class Registration : System.Web.UI.Page
+{
+
+    protected void Button1_Click(object sender, EventArgs e)
+    {
+        Response.Write("Your Registration is successful");
+    }
+    
+}
