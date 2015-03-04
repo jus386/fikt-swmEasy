635eed0a960e5a278f6e18a6283d96058533a87c
StefanijaKrsteva <krsteva.stefanija@yahoo.com>
2015-03-04 16:36:46 +0100

    login page

---
+++ FIKT.SwmEasy/Login.aspx.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data.SqlClient;
+using System.Configuration;
+
+public partial class Login : System.Web.UI.Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+
+    }
+    protected void Button_Login_Click(object sender, EventArgs e)
+    {   
+        SqlConnection conn = new SqlConnection(ConfigurationManager.ConnectionStrings["RegistrationConnectionString"].ConnectionString);
+        conn.Open();
+        string checkemail = "selectcount(*) from UserData where UserName='" + TextBoxEmail.Text + "'";
+        SqlCommand com = new SqlCommand(checkemail, conn);
+        int temp = Convert.ToInt32(com.ExecuteScalar().ToString());
+        conn.Close();
+        if (temp == 1)
+        {
+
+            conn.Open();
+            string checkPasswordQuery = "select password from UserData where UserName='" + TextBoxEmail.Text + "'";
+            SqlCommand passComm = new SqlCommand(checkPasswordQuery, conn);
+            string password = passComm.ExecuteScalar().ToString().Replace(" ", "");
+            if (password == TextBoxPassword.Text)
+            {
+                Session["New"] = TextBoxEmail.Text;
+                Response.Write("Password is correct");
+            }
+            else
+            {
+                Response.Write("Password is Not correct");
+            }
+            
+        }
+        else
+        {
+            Response.Write("Email is Not correct");
+        }
+    }
+}
